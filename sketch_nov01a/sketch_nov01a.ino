66a0650b403afca1791c4ef9d9c3d449b1ee6228
99kenny <helloiamkenny@gmail.com>
2022-12-09 05:45:49 +0900

    fix: open, cloas

--- sketch_nov01a/sketch_nov01a.ino
+++ sketch_nov01a/sketch_nov01a.ino
@@ -12,18 +12,7 @@
 #define TXD                       4
 #define RXD                       5
 
-#define RXPIN 13
-#define TXPIN 12
-//#define GPSBAUD 9600
-
-//TinyGPS gps;
-
-//gps
-//void getgps(TinyGPS &gps);
-
 SoftwareSerial bt(TXD,RXD);
-//SoftwareSerial uart_gps(RXPIN, TXPIN);
-
 
 // scale - 10Kg loadcell : 226 / 5kg loadcell : 372
 // ADC 모듈에서 측정된 결과값을 (loadcellValue)값 당 1g으로 변환해 줌
@@ -31,12 +20,17 @@ float loadcellValue = 226.0;
 
 HX711 scale;
 Servo servo;      //Servo 클래스로 servo객체 생성
+Servo servo1;
 int value = 0;    // 각도를 조절할 변수 value
+int angle = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
-  servo.attach(9);
+  servo.attach(6);
+  servo1.attach(7);
+  servo.write(0);
+  servo1.write(180);
   Serial.begin(115200);
   bt.begin(9600);
   //uart_gps.begin(GPSBAUD);
@@ -73,16 +67,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-//  while(uart_gps.available())     // While there is data on the RX pin...
-//  {
-//      int c = uart_gps.read();    // load the data into a variable...
-//      if(gps.encode(c))      // if there is a new valid sentence...
-//      {
-//        getgps(gps);         // then grab the data.
-//      }   
-//  }
 // 오프셋 및 스케일이 적용된 측정값 출력 (5회 측정 평균값) 
   Serial.print("value :\t");
   Serial.print(scale.get_units(5), 2);    // 5회 측정 평균값, 소수점 아래 2자리 출력
@@ -100,63 +84,23 @@ void loop() {
     }
     else if(flag == 'b'){
       Serial.write(1);
-      servo.write(90);
+      for(angle=0; angle < 110; angle++){
+        Serial.write(4);
+        servo.write(angle);
+        servo1.write(110 - angle);
+        delay(15);
+      }
     }
     else if(flag == 'c'){
       Serial.write(2);
-      servo.write(-90);
+      for(angle=110; angle > 0; angle--){
+        servo.write(angle);
+        servo1.write(110 - angle);
+        delay(15);
+      }
       bt.print(dtostrf(scale.get_units(5)*0.001 , 5, 2, pChrBuffer));
     }
   }
-
-
   // 1초 대기
   delay(1000);
-  
 }
-
-//void getgps(TinyGPS &gps)
-//{
-//  // To get all of the data into varialbes that you can use in your code, 
-//  // all you need to do is define variables and query the object for the 
-//  // data. To see the complete list of functions see keywords.txt file in 
-//  // the TinyGPS and NewSoftSerial libs.
-//  
-//  // Define the variables that will be used
-//  float latitude, longitude;
-//  // Then call this function
-//  gps.f_get_position(&latitude, &longitude);
-//  // You can now print variables latitude and longitude
-//  Serial.print("Lat/Long: "); 
-//  Serial.print(latitude,5); 
-//  Serial.print(", "); 
-//  Serial.println(longitude,5);
-//  
-//  // Same goes for date and time
-//  int year;
-//  byte month, day, hour, minute, second, hundredths;
-//  gps.crack_datetime(&year,&month,&day,&hour,&minute,&second,&hundredths);
-//  // Print data and time
-//  Serial.print("Date: "); Serial.print(month, DEC); Serial.print("/"); 
-//  Serial.print(day, DEC); Serial.print("/"); Serial.print(year);
-//  Serial.print("  Time: "); Serial.print(hour, DEC); Serial.print(":"); 
-//  Serial.print(minute, DEC); Serial.print(":"); Serial.print(second, DEC); 
-//  Serial.print("."); Serial.println(hundredths, DEC);
-//  //Since month, day, hour, minute, second, and hundr
-//  
-//  // Here you can print the altitude and course values directly since 
-//  // there is only one value for the function
-//  Serial.print("Altitude (meters): "); Serial.println(gps.f_altitude());  
-//  // Same goes for course
-//  Serial.print("Course (degrees): "); Serial.println(gps.f_course()); 
-//  // And same goes for speed
-//  Serial.print("Speed(kmph): "); Serial.println(gps.f_speed_kmph());
-//  Serial.println();
-//  
-//  // Here you can print statistics on the sentences.
-//  unsigned long chars;
-//  unsigned short sentences, failed_checksum;
-//  gps.stats(&chars, &sentences, &failed_checksum);
-//  //Serial.print("Failed Checksums: ");Serial.print(failed_checksum);
-//  //Serial.println(); Serial.println();
-//}
